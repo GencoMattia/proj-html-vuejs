a358c211fc660d84eda6c988eca01d7e9bf0244b
GencoMattia <gencomattia@hotmail.it>
2024-06-19 12:49:15 +0200

    linked main componets to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,29 @@
 <script>
+import MainHeader from './MainHeader.vue';
+import MainHeaderCards from './MainHeaderCards.vue';
+import MainFeatured from './MainFeatured.vue';
+import MainBenefits from './MainBenefits.vue';
+import MainGuide from './MainGuide.vue';
+import MainTopLearners from './MainTopLearners.vue';
+import MainGetStarted from './MainGetStarted.vue';
+import MainStatistics from './MainStatistics.vue';
+import MainBlogs from './MainBlogs.vue';
+import MainNewsletter from './MainNewsletter.vue';
+
 export default {
+    components: {
+        MainHeader,
+        MainHeaderCards,
+        MainFeatured,
+        MainBenefits,
+        MainGuide,
+        MainTopLearners,
+        MainGetStarted,
+        MainStatistics,
+        MainBlogs,
+        MainNewsletter,
+    },
+
     data() {
         return {
         
@@ -9,6 +33,19 @@ export default {
 </script>
 
 <template>
+    <MainHeader />
+    <MainHeaderCards />
+    <MainFeatured />
+    <MainBenefits />
+    <MainGuide />
+    <MainTopLearners />
+    <MainGetStarted />
+    <MainStatistics />
+    <MainBlogs />
+    <MainNewsletter />
+
+
+
     <h1>
         Io Sono un Main!
     </h1>
