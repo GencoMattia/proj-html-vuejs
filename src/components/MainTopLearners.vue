528e39dc48a23d4e653ec7de30d5a1d6b020b6f2
GencoMattia <gencomattia@hotmail.it>
2024-06-19 17:47:53 +0200

    Added learners data

--- src/components/MainTopLearners.vue
+++ src/components/MainTopLearners.vue
@@ -2,7 +2,32 @@
 export default {
     data() {
         return {
-        
+            learnersList: [
+                {
+                    name: "Mina Hollace",
+                    occupation: "Freelancer",
+                    title: "High level of efficiency and scientific teaching methods",
+                    text: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+                },
+                {
+                    name: "Madley Pondor",
+                    occupation: "IT Specialist",
+                    title: "Professional team of specialists and passionate mentors at reach",
+                    text: "I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+                },
+                {
+                    name: "Luvic Dubble",
+                    occupation: "Private Tutor",
+                    title: "The MaxCoach team works really hard to ensure high quality",
+                    text: "I am happy with thir arrangement of lessons and subjects. They reflect a scientific investigation into effective methods to be adopted for learners of all levels.",
+                },
+                {
+                    name: "Florence Themes",
+                    occupation: "Multimedia Admin",
+                    title: "It's a choice of quality for people with special needs",
+                    text: "I'm a very scrict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+                },
+            ]
         };
     }
 };
