9c0ddc43af9daca01109cd13071ebbeabbe9604b
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-19 16:33:58 +0200

    added structure template

--- src/components/MainFeatured.vue
+++ src/components/MainFeatured.vue
@@ -9,6 +9,34 @@ export default {
 </script>
 
 <template>
+    <section>
+        <div>
+            <p>stimulated to learn?</p>
+            <h2>Featured Online Courses</h2>
+        </div>
+        <div>
+            <article>
+                <img src="" alt="">
+                <div>
+                    <p>
+                        price
+                    </p>
+                    <h3>
+                        title
+                    </h3>
+                    <span>
+                        lessons
+                    </span>
+                    <span>
+                        students
+                    </span>
+                </div>
+            </article>
+        </div>
+        <div>
+            <button>View all courses &rightarrow;</button>
+        </div>
+    </section>
 
 </template>
 
