83b87837f6f5f9cf1b9dc9da72d3adbf075c0e35
GencoMattia <gencomattia@hotmail.it>
2024-06-19 16:02:05 +0200

    added font-awesome.js

---
+++ src/js/font-awesome.js
@@ -0,0 +1,9 @@
+import { library } from "../../node_modules/@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "../../node_modules/@fortawesome/vue-fontawesome";
+import { fas } from "../../node_modules/@fortawesome/free-solid-svg-icons";
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fal } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fal);
+
+export { FontAwesomeIcon };
