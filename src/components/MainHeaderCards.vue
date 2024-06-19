2d0cbcadea896e62d8df065e98820fc3c19959ce
GencoMattia <gencomattia@hotmail.it>
2024-06-19 16:39:41 +0200

    MainHeaderCards_added clientsCards data

--- src/components/MainHeaderCards.vue
+++ src/components/MainHeaderCards.vue
@@ -2,14 +2,40 @@
 export default {
     data() {
         return {
-        
+            clientsCards: [
+                {
+                    icon: "client-logo-01.png"
+                },
+                {
+                    icon: "client-logo-02.png"
+                },
+                {
+                    icon: "client-logo-03.png"
+                },
+                {
+                    icon: "client-logo-04.png"
+                },
+                {
+                    icon: "client-logo-05.png"
+                },
+                {
+                    icon: "client-logo-06.png"
+                },
+                {
+                    icon: "client-logo-07.png"
+                }
+            ]
         };
     }
 };
 </script>
 
 <template>
+    <div class="my-container"> 
+        <article>
 
+        </article>
+    </div>
 </template>
 
 <style scoped>
