2055b3273ac65f7223778a644d3436c7ae0d8b02
GencoMattia <gencomattia@hotmail.it>
2024-06-20 10:50:17 +0200

    set a computed to see only 3 articles. Imported FontAwesomeArticle

--- src/components/MainTopLearners.vue
+++ src/components/MainTopLearners.vue
@@ -1,4 +1,6 @@
 <script>
+import { FontAwesomeIcon } from '../js/font-awesome';
+
 export default {
     data() {
         return {
@@ -9,6 +11,7 @@ export default {
                     occupation: "Freelancer",
                     title: "High level of efficiency and scientific teaching methods",
                     text: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+                    isVisible: false,
                 },
                 {
                     avatar:"testimonial-avata-02.jpg",
@@ -16,6 +19,7 @@ export default {
                     occupation: "IT Specialist",
                     title: "Professional team of specialists and passionate mentors at reach",
                     text: "I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+                    isVisible: false,
                 },
                 {
                     avatar:"testimonial-avata-03.jpg",
@@ -23,6 +27,7 @@ export default {
                     occupation: "Private Tutor",
                     title: "The MaxCoach team works really hard to ensure high quality",
                     text: "I am happy with thir arrangement of lessons and subjects. They reflect a scientific investigation into effective methods to be adopted for learners of all levels.",
+                    isVisible: false,
                 },
                 {
                     avatar:"testimonial-avata-04.jpg",
@@ -30,8 +35,12 @@ export default {
                     occupation: "Multimedia Admin",
                     title: "It's a choice of quality for people with special needs",
                     text: "I'm a very scrict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+                    isVisible: false,
                 },
-            ]
+
+            ],
+
+            activeArticle: 0,
         };
     },
 
@@ -39,13 +48,29 @@ export default {
         getImagePath: function(img) {
             return new URL(`../assets/img/${img}`, import.meta.url).href;
         }
+    },
+
+    computed: {
+        visibleCards() {
+            const index = this.activeArticle;
+            const count = this.learnersList.length;
+
+            const prevIndex = (index - 1 + count) % count;
+            const nextIndex = (index + 1) % count;
+
+            return [
+                this.learnersList[prevIndex],
+                this.learnersList[index],
+                this.learnersList[nextIndex]
+            ];
+        }
     }
 };
 </script>
 
 <template>
-    <section class="testimonials row flex-nowrap overflow-hidden align-items-stretch">
-        <article v-for="(card, index) in learnersList" :key="index" class="testimonal-card col-4">
+    <section class="testimonials row flex-nowrap overflow-hidden align-items-stretch justify-content-evenly">
+        <article v-for="(card, index) in visibleCards" :key="index" class="testimonal-card col-3 p-1 d-flex flex-column justify-content-between">
             <div class="card-content">
                 <h2>
                     {{ card.title }}
