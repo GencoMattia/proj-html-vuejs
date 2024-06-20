54ba4805065f5ce67affaaa03ebd068aaf2c8641
GencoMattia <gencomattia@hotmail.it>
2024-06-20 17:51:45 +0200

    called API throught fetchEvent method

--- src/components/EventsPage.vue
+++ src/components/EventsPage.vue
@@ -1,9 +1,28 @@
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
         
         };
+    },
+
+    methods: {
+        fetchEvents() {
+            axios.get('http://152.89.170.170:3000/events/elearning')
+                .then((response) => {
+                    console.log(response.data);
+                    this.events = response.data;
+                })
+                .catch((error) => {
+                    console.error("Error fetching events:", error);
+                });
+        }
+    },
+
+    mounted() {
+        this.fetchEvents();
     }
 };
 </script>
