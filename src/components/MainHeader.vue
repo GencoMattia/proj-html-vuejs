254fad961641e5f4ab5db346336b644ef4954526
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-06-20 10:41:14 +0200

    Rimosso prova movimento img

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,26 +2,8 @@
 export default {
     data() {
         return {
-        movementStrength: 25,
-        w: window.innerWidth,
-        h: window.innerHeight,
-        newvalueX: 0,
-        newvalueY: 0
-    }},
-    methods: {
-        handleMouseMove(e) {
-            const pageX = (e.pageX - this.w / 2) / this.w / 2;
-            const pageY = (e.pageY - this.h / 2) / this.h / 2;
-            this.newvalueX = pageX * this.movementStrength;
-            this.newvalueY = pageY * this.movementStrength;
-        }
-    },
-    mounted() {
-        window.addEventListener('mousemove', this.handleMouseMove);
-    },
-    beforeDestroy() {
-        window.removeEventListener('mousemove', this.handleMouseMove);
-    }
+
+    }}
 }
 </script>
 
@@ -37,7 +19,7 @@ export default {
                         Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process.
                     </p>
                     <button>
-                        Download free guide
+                        <font-awesome-icon icon="fa-solid fa-download" /> Download free guide
                     </button>
                     <p>
                         Have questions? <a href="#">Get Free Sample</a>
@@ -46,8 +28,8 @@ export default {
                 <div class="col-6 ciao">
                         <img src="../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
                     <div class="my_bg-move">
-                        <div ref="topImage" class="top-image" :style="{ left: newvalueX + 'px', top: newvalueY + 'px' }"></div>
-                    </div>
+                        <div class="top-image"></div>
+                    </div>``
                 </div>
             </section>
         </div>
@@ -74,8 +56,5 @@ export default {
     z-index: 0;
     height: 100%;
     background-repeat: no-repeat;
-    }
-    .ciao{
-        position: relative;
     }
 </style>
