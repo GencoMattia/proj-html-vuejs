41be8bcc307c6eac44a1eaa0812f453939efbb6e
GencoMattia <gencomattia@hotmail.it>
2024-06-20 18:08:28 +0200

    Set up cards framework; added event img

--- src/components/EventsPage.vue
+++ src/components/EventsPage.vue
@@ -42,11 +42,15 @@ export default {
                 <div class="events-filter">
 
                 </div>
-                <div class="events-cards-wrapper">
+                <div v-for="(event, index) in eventsList" :key="index" class="events-cards-wrapper">
                     <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img :src="event.image" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text event-date">
+                                
+                            </p>
+                            <p class="card-text event-name">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text event-location">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
