a17ee9d42067c7d65d60437dfcb346c80cf1498f
GencoMattia <gencomattia@hotmail.it>
2024-06-21 12:21:42 +0200

    MainGetStarted: added base html framework

--- src/components/MainGetStarted.vue
+++ src/components/MainGetStarted.vue
@@ -9,9 +9,20 @@ export default {
 </script>
 
 <template>
+    <section class="my-container get-started">
+        <h2>
+            Start today for getting <span>Online Certification</span>
+        </h2>
 
+        <h1>
+            You can be your own guiding star with our help!
+        </h1>
+        <button class="get-started-button">
+            Get started now
+        </button>
+    </section>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
