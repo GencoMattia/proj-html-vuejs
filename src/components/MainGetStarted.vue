c28b9f05f1361231bb0a932384b65cb731355423
GencoMattia <gencomattia@hotmail.it>
2024-06-21 12:43:08 +0200

    added position absolute to the background div

--- src/components/MainGetStarted.vue
+++ src/components/MainGetStarted.vue
@@ -61,8 +61,14 @@ section {
         }
     }
     
-    
-    
+    .background-landscape {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1;
+    }
 }
 
 
