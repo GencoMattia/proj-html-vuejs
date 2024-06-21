69046376e343213012ea6498e512fc9d691c9b67
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-06-21 09:45:01 +0200

    Spostato i dati da mainheader to main guide

--- src/components/MainGuide.vue
+++ src/components/MainGuide.vue
@@ -1,17 +1,66 @@
 <script>
+import { FontAwesomeIcon } from '../js/font-awesome.js';
 export default {
+    components:{
+        FontAwesomeIcon
+    },
     data() {
         return {
-        
-        };
-    }
-};
+            
+    }}
+}
 </script>
 
 <template>
-
+    <div class="my-container">
+        <div class="container">
+            <section class="row">
+                <div class="col-6 position-relative">
+                        <img src="../assets/img/home-2-popup-video-poster.jpg" alt="">
+                    <div class="my_bg-move">
+                        <div class="top-image"></div>
+                    </div>
+                </div>
+                <article class="col-6">
+                    <p class="my_cl-blue">
+                        GET STARTED <span class="fw-bold">EFFORTLESSLY</span>
+                    </p>
+                    <h2>
+                        <span class="my_cl-blue fw-bold">Enjoy Our</span> <br>
+                        <span class="my_cl-green">Companionship</span>
+                    </h2>
+                    <p>
+                        Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some hidden hints of early-bird rewards as well.
+                    </p>
+                    <p>
+                        <a href="#" class="my_cl-green fw-bold">Download free guidebook <font-awesome-icon :icon="['fas', 'arrow-right']"/></a> 
+                    </p>
+                </article>
+                
+            </section>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
+    img{
+        width: 100%;
+        border-radius: 5px;
+    }
+    .top-image {
+    content: url('../assets/img/icon-youtube-play.png');
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 15%;
+    }
+    .my_cl-green{
+        color: #20ad96;
+    }
+    .my_cl-blue{
+        color: #3f3a64;
+    }
 </style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,65 +1,17 @@
 <script>
-import { FontAwesomeIcon } from '../js/font-awesome.js';
 export default {
-    components:{
-        FontAwesomeIcon
-    },
     data() {
         return {
-            
-    }}
-}
+        
+        };
+    }
+};
 </script>
 
 <template>
-    <div class="my-container">
-        <div class="container">
-            <section class="row">
-                <div class="col-6 ciao">
-                        <img src="../assets/img/about-me-profile-ohm4dxoearqbxny7q3bq1pzbdgofdx0tobbeqcvzd0.jpg" alt="">
-                    <div class="my_bg-move">
-                        <div class="top-image"></div>
-                    </div>``
-                </div>
-                <article class="col-6">
-                    <h2>
-                        Distant learning for further expansion
-                    </h2>
-                    <p>
-                        Learning is a life-long journey that in fact we never find the terminate stop. Stop searching, enjoy the process.
-                    </p>
-                    <button>
-                        <font-awesome-icon :icon="['fas', 'download']" /> Download free guide
-                    </button>
-                    <p>
-                        Have questions? <a href="#">Get Free Sample</a>
-                    </p>
-                </article>
-                
-            </section>
-        </div>
-    </div>
+
 </template>
 
 <style scoped>
-    img{
-        border-radius: 50%;
-    }
-    .my_bg-move {
-    padding: 25px;
-    width: 35%;
-    height: 35%;
-    position: absolute;
-    top: 100px;
-    }
 
-    .top-image {
-    background: url('../assets/img/maxcoach-shape-02.png');
-    position: absolute;
-    background-size: contain;
-    width: 25%;
-    z-index: 0;
-    height: 100%;
-    background-repeat: no-repeat;
-    }
 </style>
