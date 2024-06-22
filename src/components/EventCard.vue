7117a48942e8beec280349bcb3da5f98968c2387
GencoMattia <gencomattia@hotmail.it>
2024-06-22 18:37:16 +0200

    successfully managed single event card with props

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,11 @@
 <script>
+import { FontAwesomeIcon } from '../js/font-awesome';
+
 export default {
+    components: {
+        FontAwesomeIcon,
+    },
+
     data() {
         return {
         
@@ -39,6 +45,66 @@ export default {
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "../styles/partials/mixins" as *;
 
+    .card {
+        width: calc((100% / 3) - 1.35rem) !important;
+        margin-bottom: 2rem;
+        text-align: center;
+        display: flex;
+        overflow: hidden;
+
+        &:hover img{
+            transform: scale(1.1);
+        }
+
+        &:hover .overlay {
+            opacity: 1 !important;
+        }
+
+        .image-container {
+            overflow: hidden;
+            position: relative;
+
+            .overlay {
+                @include flex-centered;
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                background-color: rgba(63,58,100,0.6);
+                opacity: 0;
+                transition: opacity 0.3s ease;
+
+                button {
+                    font-weight: bolder;
+                    color: #20ad96;
+                    border: none;
+                    padding: .5rem 2rem;
+                    border-radius: .5rem;
+                }
+            }
+
+            .card-img-top {
+                transition: transform 0.6s ease;
+            }
+        }
+
+        .event-date {
+            color: #20ad96;
+            @include uppercase-text;
+            font-weight: bold;
+        }
+
+        .event-name {
+            font-weight: bold;
+            font-size: 1.2rem;
+        }
+
+        .event-location {
+            color: #7e7e7e;
+        }
+    }
 </style>
